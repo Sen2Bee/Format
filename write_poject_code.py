41d4cb4bad12f3e35737b986251df67a8d646335
thomas.koschitzki <thomas.koschitzki@julius-kuehn.de>
2024-10-26 20:13:13 +0200

    movie_details_poster - text flow 1

--- write_poject_code.py
+++ write_poject_code.py
@@ -9,10 +9,10 @@ output_file = os.path.join(root_path, 'code.txt')
 
 # Define text-based file extensions to include
 text_extensions = {
-    # '.py', 
-    # '.html', 
+    '.py', 
+    '.html', 
     '.css', 
-    # '.js'
+    '.js'
     }
 
 # Define directories and files to include
